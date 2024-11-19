1953c7ceac844bb8b2698e0676860cb377f153d8
simtight\2312018 <simtight1108@gmail.com>
2024-11-19 11:30:05 +0900

    浮いてる敵の名前ふよふよ

---
+++ Assets/Scripts/FuyoFuyoMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuyoFuyoMovement : MonoBehaviour
+{
+    [SerializeField]private Rigidbody2D rb;
+    [SerializeField] private GameObject playerObject;
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
